8ff18e82ae9d9e6618e5d6ff126d3c2d28979fc3
Maksym_Musiichuk <maksym.musiichuk@gmail.com>
2021-04-22 22:17:13 +0300

    Refactoring

--- product-service/src/handlers/getProducts.test.js
+++
@@ -1,8 +0,0 @@
-import { getProducts } from './getProducts';
-
-describe('getProduct', () => {
-    test('should return statusCode 200', async () => {
-        const response = await getProducts();
-        expect(response.statusCode).toEqual(200);
-    });
-});
--- product-service/src/services/getAllProducts/getAllProducts.test.js
+++
@@ -1,9 +0,0 @@
-import productsMock from '../../productList.json';
-import { getAllProducts } from './getAllProducts';
-
-describe('getAllProducts', () => {
-    test('should return all hardcoded products', async () => {
-        const products = await getAllProducts();
-        expect(products).toEqual(productsMock);
-    })
-})
